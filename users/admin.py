1c0dd71706735f7faa07a6226c4e36871851d02c
ted <tngeene27@gmail.com>
2020-08-22 22:26:23 +0300

    added average rating in artist profile details and list view

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserAccount,ArtistProfile,CollectorProfile
+from .models import UserAccount,ArtistProfile,CollectorProfile, Rating
 # Register your models here.
 
 admin.site.register(UserAccount)
 admin.site.register(ArtistProfile)
 admin.site.register(CollectorProfile)
+admin.site.register(Rating)
