558a4ba512e0cabbb3dc89c1b1dc198720bb6885
ted <tngeene27@gmail.com>
2020-09-23 16:06:13 +0300

    redesigned profile page

--- users/apiv1/serializers/artists_serializer.py
+++ users/apiv1/serializers/artists_serializer.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from users.models import ArtistProfile, Rating
-from django.db.models import Avg
+from django.db.models import Avg, Sum
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class ArtistRatingSerializer(ModelSerializer):
@@ -9,7 +12,8 @@ class ArtistRatingSerializer(ModelSerializer):
 
     class Meta:
         model = Rating
-        fields = ('id','rating','comment', 'posted_by', 'created_at', 'updated_on')
+        fields = ('id', 'rating', 'comment', 'posted_by',
+                  'created_at', 'updated_on')
 
 
 class ArtistProfileCreateSerializer(ModelSerializer):
@@ -20,28 +24,44 @@ class ArtistProfileCreateSerializer(ModelSerializer):
         fields = '__all__'
 
 
+class ArtistProfileUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name', 'phone_number', 'email')
+
 class ArtistProfileListSerializer(ModelSerializer):
-    user = serializers.StringRelatedField()
+    user = ArtistProfileUserSerializer()
     average_rating = serializers.SerializerMethodField()
+    ratings = serializers.SerializerMethodField()
+
     class Meta:
         model = ArtistProfile
-        fields = ('id', 'bio', 'user', 'county','birth_place',
-                'photo', 'average_rating','created_on', 'updated_on')
+        fields = ('id', 'bio', 'user', 'county', 'birth_place',
+                  'photo', 'ratings', 'average_rating', 'created_on', 'updated_on')
 
     def get_average_rating(self, obj):
         avg_rating = obj.ratings.all().aggregate(Avg('rating'))
         return avg_rating['rating__avg']
 
+    def get_ratings(self, obj):
+        total_ratings = obj.ratings.all().count()
+        return total_ratings
+
+
 class ArtistProfileDetailSerializer(ModelSerializer):
-    user = serializers.StringRelatedField()
-    ratings = ArtistRatingSerializer(many=True)
+    user = ArtistProfileUserSerializer()
+    ratings = serializers.SerializerMethodField()
     average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = ArtistProfile
         fields = ('id', 'bio', 'user', 'county',
-                  'birth_place', 'photo', 'ratings','average_rating')
+                  'birth_place', 'photo', 'ratings', 'average_rating')
 
     def get_average_rating(self, obj):
         avg_rating = obj.ratings.all().aggregate(Avg('rating'))
         return avg_rating['rating__avg']
+
+    def get_ratings(self, obj):
+        total_ratings = obj.ratings.all().count()
+        return total_ratings
